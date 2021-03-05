d2f1fe94aecf0343455246c0f07ca5e1df7531d2
Zhandor Gonzaga <zhandorlg@gmail.com>
2021-03-05 17:19:46 -0300

    acerto nas pastas e criado modulo user

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUser(): string {
+    console.log('user.controller.ts = getUser');
+    return this.userService.getUser();
+  }
+}
